7697eb111fb7dc5c814c1385307c7f21fe05151d
“davegd1387” <davegd1387@gmailcom>
2022-08-03 13:15:11 -0400

    First major commit

--- src/App.js
+++ src/App.js
@@ -1,56 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+
+import Header  from './userFiles/Header.js';
+import TopHeader  from './components/TopHeader.js';
+import SearchBar  from './components/SearchBar.js';
+import HomeReddits  from './components/HomeReddits';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <TopHeader />
+      <Header />
+      <SearchBar />
+      <HomeReddits />
+      
     </div>
   );
 }
